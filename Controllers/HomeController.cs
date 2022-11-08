0213546de3d9813edc9873ca308a52b292ae1124
CrysD5 <matrim.ivy@gmail.com>
2022-11-08 03:01:19 -0500

    student ddl created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KendoUIDropDownListApp.Models;
 
 namespace KendoUIDropDownListApp.Controllers
 {
     public class HomeController : Controller
     {
+        public Data.CanvasAPI.CanvasAPIEntities canEnt = new Data.CanvasAPI.CanvasAPIEntities();
+        public Data.Registrar.RegistrarEntities regEnt = new Data.Registrar.RegistrarEntities();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -27,6 +31,17 @@ namespace KendoUIDropDownListApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public JsonResult GetStudents()
+        {
+            var students = canEnt.v_studentsAll.Select(x => new StudentModel
+            {
+                studentname = x.studentname,
+                emaddr = x.emaddr
+            });
+
+            return Json(students, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Models/StudentModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KendoUIDropDownListApp.Controllers;
+
+namespace KendoUIDropDownListApp.Models
+{
+    public class StudentModel
+    {
+        public string Emplid { get; set; }
+        public string Last_Name { get; set; }
+        public string First_Name { get; set; }
+        public string Middle_Name { get; set; }
+        public string Sex { get; set; }
+        public string Ethnic_Grp_CD { get; set; }
+        public Nullable<System.DateTime> BIRTHDATE { get; set; }
+        public string studentname { get; set; }
+        public string Email_Addr { get; set; }
+        public string emaddr { get; set; }
+        public string mdyr { get; set; }
+        public string gyr { get; set; }
+        public string campuscode { get; set; }
+        public string Acad_Prog { get; set; }
+        public string desk { get; set; }
+        public string advisor_name { get; set; }
+        public string advisor_email { get; set; }
+        public string advisor_campus_id { get; set; }
+        public string Prog_status { get; set; }
+        public string CW_PREF_FIRST_NAME { get; set; }
+        public string reduced_fee { get; set; }
+        public string gender_identity { get; set; }
+        public string gender_pronoun { get; set; }
+    }
+}
